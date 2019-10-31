80b168ac1526854f689f6dd22bae26c439f3462c
AimeeNavik <aimeenavik@gmail.com>
2019-10-31 14:27:41 -0600

    feat: add login and dashboard views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Panel de control</div>
+<div class="dashboard">
+    <h1>Panel de control</h1>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Bienvenido
-                </div>
+    <div class="container">
+        <div class="row section">
+            <div class="col-sm">
+                <i class="fa fa-users"></i>
+                <p class="section-title"><a href="#">Usuarios</a></p>
+            </div>
+            <div class="col-sm">
+                <i class="fa fa-tags"></i>
+                <p class="section-title"><a href="#">Clasificación</a></p>
+            </div>
+            <div class="col-sm">
+                <i class="fa fa-book"></i>
+                <p class="section-title"><a href="#">Libros</a></p>
+            </div>
+        </div>
+        <div class="row section">
+            <div class="col-sm">
+                <i class="fa fa-cubes"></i>
+                <p class="section-title"><a href="#">Materiales</a></p>
+            </div>
+            <div class="col-sm">
+                <i class="fa fa-university"></i>
+                <p class="section-title"><a href="#">Editoriales</a></p>
+            </div>
+            <div class="col-sm">
+                <i class="fa fa-newspaper-o"></i>
+                <p class="section-title"><a href="#">Autores</a></p>
+            </div>
+        </div>
+        <div class="row section">
+            <div class="col-sm">
+                <i class="fa fa-handshake-o"></i>
+                <p class="section-title"><a href="#">Prestamos</a></p>
+            </div>
+            <div class="col-sm">
+                <i class="fa fa-money"></i>
+                <p class="section-title"><a href="#">adeudos</a></p>
+            </div>
+            <div class="col-sm">
+                <i class="fa fa-line-chart"></i>
+                <p class="section-title"><a href="#">reportes</a></p>
+            </div>
+        </div>
+        <div class="row section">
+            <div class="col-sm">
+                <i class="fa fa-graduation-cap"></i>
+                <p class="section-title"><a href="#">Carreras</a></p>
+            </div>
+            <div class="col-sm">
+                <i class="fa fa-qrcode"></i>
+                <p class="section-title"><a href="#">Código de libro</a></p>
             </div>
         </div>
     </div>
 </div>
+
+
 @endsection
