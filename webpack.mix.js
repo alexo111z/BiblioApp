bdae9fa913222c8c76f3d205f6dee9fadf324b80
AlanSanchezTics <alan.dsrn@gmail.com>
2019-11-03 17:12:55 -0600

    acomodando scripts reportes 03-11-19

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,15 +10,17 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+//General
+mix.styles([
+    'resources/css/bootstrap.css',
+    'resources/css/toastr.css',
+    ], 'public/css/app.css');
+//reportes
 mix.scripts([
     'resources/js/jquery.js',
     'resources/js/bootstrap.js',
     'resources/js/toastr.js',
     'resources/js/vue.js',
     'resources/js/axios.js',
-    'resources/js/app.js'], 'public/js/reportes/app.js')
-    .styles([
-    'resources/css/bootstrap.css',
-    'resources/css/toastr.css',
-    ], 'public/css/app.css');
+    'resources/js/reportes/app.js'], 'public/js/reportes/app.js')
+
