0619cf299bb24a5e2d30ff29c1b70b91c7da0c00
Kari <karina.pola444@gmail.com>
2019-11-27 13:15:04 -0600

    selectClaveOk

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,9 @@ Route::get('/carrera', function(){
 });
 //Materiales
 Route::resource('material', 'MaterialesController', ['except' =>'show','create', 'edit']);
+
 Route::get('/materiales', function (){
     return view('Materiales.principal');
 });
+
+Route::get('materiales', 'MaterialesController@cla')->name('clavesCarreras');
